08db735cb402a1426aa24e8292af579475fd5424
Jeff Berlin <jeff@jbrally.com>
2017-02-07 17:24:54 -0500

    Geolocation and autocomplete are working
    Geolocation and autocomplete are working as well as markers from
    searches with the infowindow.

--- Google Maps/app/js/main.js
+++ Google Maps/app/js/main.js
@@ -5,10 +5,8 @@ var styles = require('./styles').styles
  //function AppViewModel() {
 
       var map;
-
       var markers = [];
-
-      var placeMarkers = [];
+      var infowindow;
 
       //var wilm = { lat: 34.2257, lng: -77.9447 };
 
@@ -16,40 +14,88 @@ var styles = require('./styles').styles
 
         map = new google.maps.Map(document.getElementById('map'), {
           //center: wilm, //{lat: 34.2257, lng: -77.9447},
-          zoom: 14,
+          zoom: 11,
           styles: styles,
           mapTypeConrtol: false
         });
 
+        // GEOLOCATION:
+        
         var infoWindow = new google.maps.InfoWindow({map: map});
-
-        // geolocation.
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-
-              infoWindow.setPosition(pos);
-              infoWindow.setContent('YOU ARE HERE');
-              map.setCenter(pos);
-            }, function() {
-              handleLocationError(true, infoWindow, map.getCenter());
-            });
-          } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-          }
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('YOU ARE HERE!');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          })
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
         }
 
-        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        function handleLocationErro(browserHasGeolocation, infoWindow, pos) {
           infoWindow.setPosition(pos);
           infoWindow.setContent(browserHasGeolocation ?
-                                'Error: The Geolocation service failed.' :
-                                'Error: Your browser doesn\'t support geolocation.');
-    
-    }
+            'Error: The Geolocation service failed.' :
+            'Error: Please enable Google to access your location.');
+        }
+
+        // AUTOCOMPLETE:
+
+        var input = document.getElementById('pac-input');
+        var card = document.getElementById('container');
+        var autocomplete = new google.maps.places.Autocomplete(input);
+
+        autocomplete.bindTo('bounds', map);
+
+        var infowindow = new google.maps.InfoWindow();
+        var infowindowContent = document.getElementById('infowindow-content');
+        infowindow.setContent(infowindowContent);
+
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
+          // present on map if place has geometry
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(13);
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+          infowindowContent.children['place-icon'].src = place.icon;
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-address'].textContent = address;
+          infowindow.open(map, marker);
+        });
+
+      }
     window.initMap = initMap
         //model
       //   var Location = function(data) {
