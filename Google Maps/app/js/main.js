ff7825a5b94dfd235e7ea98bf91d2f422279eaa1
Jeff Berlin <jeff@jbrally.com>
2017-02-08 14:38:26 -0500

    Changed some styling
    Changed some of the styling for the left sidebar

--- Google Maps/app/js/main.js
+++ Google Maps/app/js/main.js
@@ -14,10 +14,10 @@ var styles = require('./styles').styles
       function initMap() {
 
         map = new google.maps.Map(document.getElementById('map'), {
-          //center: wilm, //{lat: 34.2257, lng: -77.9447},
+          //center: wilm, {lat: 34.2257, lng: -77.9447},
           zoom: 14,
           styles: styles,
-          mapTypeConrtol: false
+          //mapTypeConrtol: false
         });
 
         // GEOLOCATION:
@@ -47,63 +47,14 @@ var styles = require('./styles').styles
             'Error: Please enable Google to access your location.');
         }
 
-        // AUTOCOMPLETE:
-
-        var input = document.getElementById('pac-input');
-        var card = document.getElementById('container');
-        var autocomplete = new google.maps.places.Autocomplete(input);
-
-        autocomplete.bindTo('bounds', map);
-
-        var infowindow = new google.maps.InfoWindow();
-        var infowindowContent = document.getElementById('infowindow-content');
-        infowindow.setContent(infowindowContent);
-
-        var marker = new google.maps.Marker({
-          map: map,
-          animation: google.maps.Animation.DROP,
-          anchorPoint: new google.maps.Point(0, -29)
-        });
-
-        autocomplete.addListener('place_changed', function() {
-          infowindow.close();
-          marker.setVisible(false);
-          var place = autocomplete.getPlace();
-          if (!place.geometry) {
-            window.alert("No details available for input: '" + place.name + "'");
-            return;
-          }
-          // present on map if place has geometry
-          if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-          } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(13);
-          }
-          marker.setPosition(place.geometry.location);
-          marker.setVisible(true);
-
-          var address = '';
-          if (place.address_components) {
-            address = [
-            (place.address_components[0] && place.address_components[0].short_name || ''),
-            (place.address_components[1] && place.address_components[1].short_name || ''),
-            (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-          }
-          infowindowContent.children['place-icon'].src = place.icon;
-          infowindowContent.children['place-name'].textContent = place.name;
-          infowindowContent.children['place-address'].textContent = address;
-          infowindow.open(map, marker);
-        });
-
-        // APPEND NEARBY RESTAURANTS:
+        // APPEND NEARBY Night Clubs and Bars (5 mile radius):
         
         var service = new google.maps.places.PlacesService(map);
         service.nearbySearch({
-          location: LatLng,
-          radius: 500,
-          types: ['food', 'bar', 'restaurant', 'cafe']
+          location: wilm, // <-- NEED SOMETHING FOR LOCATION
+          radius: 3218.69,
+          types: ['night_club', 'bar', 'meal_takeaway', 'meal_delivery', 'cafe', 'restaurant'],
+          results: ['food']
         }, callback);
 
         function callback(results, status) {
@@ -128,6 +79,65 @@ var styles = require('./styles').styles
           });
         }
 
+        // AUTOCOMPLETE:
+
+        var input = document.getElementById('pac-input');
+        var card = document.getElementById('container');
+        var autocomplete = new google.maps.places.Autocomplete(input);
+
+        autocomplete.bindTo('bounds', map);
+
+        var infowindow = new google.maps.InfoWindow();
+        var infowindowContent = document.getElementById('infowindow-content');
+        infowindow.setContent(infowindowContent);
+
+
+
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
+          // present on map if place has geometry
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(15);
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+          infowindowContent.children['place-icon'].src = place.icon;
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-address'].textContent = address;
+          infowindow.open(map, marker);
+        });
+
+        var icon = {
+          // url: place.icon,
+          size: new google.maps.Size(35, 35),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(15, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+        var marker = new google.maps.Marker({
+          map: map,
+          //icon: icon,
+          animation: google.maps.Animation.DROP,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
 
       }
     window.initMap = initMap
