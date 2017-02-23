31b5c2e7a38f6eddfb57d3da376c1704eb17f572
Jeff Berlin <jeff@jbrally.com>
2017-02-23 11:09:48 -0500

    Changed to coffee shops and appended markers
    Changed to coffee shops in Wilm, NC and appended the markers for shops.

--- Google Maps/app/js/main.js
+++ Google Maps/app/js/main.js
@@ -4,7 +4,7 @@ var styles = require('./styles').styles
 
    function AppViewModel() {
          this.header = ko.observable("Search For Location");
-         this.parks = ko.observable("Local Parks:");
+         this.listTitle = ko.observable("Local Coffee Shops:");
 
      }
      ko.applyBindings(new AppViewModel());
@@ -22,22 +22,39 @@ var styles = require('./styles').styles
          mapTypeConrtol: false
        });
 
-       var parks = [
-         {title: 'Robert Strange Park', location: {lat: 34.230745, lng: -77.937444}},
-         {title: 'Claude Howell Park', location: {lat: 34.232732, lng: -77.949375}},
-         {title: 'Hugh McRae Park', location: {lat: 34.208258, lng: -77.879051}},
-         {title: 'Portia Hines Park', location: {lat: 34.240406, lng: -77.936905}},
-         {title: 'Greenfield Park', location: {lat: 34.214281, lng: -77.944126}},
-         {title: 'Empire Park', location: {lat: 34.220848, lng: -77.902875}},
-         {title: 'Maides Park', location: {lat: 34.249825, lng: -77.901473}},
-         {title: 'Story Park', location: {lat: 34.238018, lng: -77.946599}}
+       var coffeeShops = [
+         {title: 'Port City Java', location: {lat: 34.235912, lng: -77.948826}},
+         {title: 'Port City Java', location: {lat: 34.238930, lng: -77.949028}},
+         {title: 'Port City Java', location: {lat: 34.237872, lng: -77.921174}},
+         {title: 'Port City Java', location: {lat: 34.201974, lng: -77.922590}},
+         {title: 'Port City Java', location: {lat: 34.242096, lng: -77.863673}},
+         {title: 'Port City Java', location: {lat: 34.194293, lng: -77.910822}},
+         {title: 'Bespke Coffee & Dry Goods', location: {lat: 34.236453, lng: -77.947403}},
+         {title: '24 South Coffee House', location: {lat: 34.234496, lng: -77.948725}},
+         {title: 'Karen\'s Cafe', location: {lat: 34.238730, lng: -77.948981}},
+         {title: 'Java Dog Coffee House', location: {lat: 34.239104, lng: -77.949228}},
+         {title: 'Folks on Fourth', location: {lat: 34.243700, lng: -77.945501}},
+         {title: 'Brick + Mortar Coffee and Supply', location: {lat: 34.247251, lng: -77.946280}},
+         {title: 'Folks Cafe', location: {lat: 34.237704, lng: -77.934188}},
+         {title: 'Luna Caffè', location: {lat: 34.228263, lng: -77.940812}},
+         {title: 'Morning Glory Coffeehouse', location: {lat: 34.225831, lng: -77.929120}},
+         {title: 'Starbucks', location: {lat: 34.216803, lng: -77.906956}},
+         {title: 'Starbucks', location: {lat: 34.242066, lng: -77.828570}},
+         {title: 'Starbucks', location: {lat: 34.196443, lng: -77.890236}},
+         {title: 'Zola Coffee & Tea', location: {lat: 34.213228, lng: -77.887951}},
+         {title: 'Addicted to the Bean', location: {lat: 34.213678, lng: -77.886954}},
+         {title: 'Grinders Caffè', location: {lat: 34.212560, lng: -77.871677}},
+         {title: 'Daily Grind', location: {lat: 34.241911, lng: -77.867955}},
+         {title: 'Bitty & Beau\'s Coffee', location: {lat: 34.242041, lng: -77.877485}},
+         {title: 'Lucky Joe Craft Coffee', location: {lat: 34.266057, lng: -77.837758}}
+         
        ];
 
        var infowindow = new google.maps.InfoWindow();
 
-       for (var i = 0; i < parks.length; i++) {
-         var position = parks[i].location;
-         var title = parks[i].title;
+       for (var i = 0; i < coffeeShops.length; i++) {
+         var position = coffeeShops[i].location;
+         var title = coffeeShops[i].title;
          var marker = new google.maps.Marker({
            position: position,
            title: title,
@@ -53,9 +70,9 @@ var styles = require('./styles').styles
          })
        }
 
-       document.getElementById('show-parks').addEventListener('click', showMarkers);
+       document.getElementById('show-shops').addEventListener('click', showMarkers);
 
-       document.getElementById('hide-parks').addEventListener('click', function() {
+       document.getElementById('hide-shops').addEventListener('click', function() {
          hideMarkers(markers);
        });
 
