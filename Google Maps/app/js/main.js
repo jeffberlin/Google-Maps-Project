b86f2c69411f9d33892f9da9c1c739cc32b8744f
Jeff Berlin <jeff@jbrally.com>
2017-03-07 13:15:47 -0500

    Rewriting the code
    Rewriting the code. Right now, the map shows with list of shops
    appended, but not markers and nothing is clickable.

--- Google Maps/app/js/main.js
+++ Google Maps/app/js/main.js
@@ -3,55 +3,35 @@ var ko = require("knockout")
 var styles = require('./styles').styles
 
    function AppViewModel() {
-         this.header = ko.observable("Search For Location");
-         this.local = ko.observable("Local Coffee Shops:");
+      this.header = ko.observable("Search For Location");
+      this.local = ko.observable("Local Coffee Shops:");
 
-         var self = this;
+      // ViewModel
+      var self = this;
          self.shops = ko.observableArray([
-          'Port City Java',
-          'Starbucks',
-          '24 South Coffee House',
-          'Karen\'s Cafe',
-          'Luna Caffè',
-          'Folks on Fourth',
-          'Folks Cafe',
-          'Zola Coffee & Tea',
-          'Grinders Caffè',
-          'Daily Grind',
-          'Addicted to the Bean',
-          'Bitty & Beau\'s Coffee',
-          'Lucky Joe Craft Coffee',
-          'Java Dog Coffee House',
-          'Morning Glory Coffeehouse',
-          'Bespoke Coffee & Dry Goods',
-          'Brick + Mortar Coffee and Supply'
+          { title: 'Port City Java' },
+          { title: 'Starbucks' },
+          { title: '24 South Coffee House' },
+          { title: 'Karen\'s Cafe' },
+          { title: 'Luna Caffè' },
+          { title: 'Folks on Fourth' },
+          { title: 'Folks Cafe' },
+          { title: 'Zola Coffee & Tea' },
+          { title: 'Grinders Caffè' },
+          { title: 'Daily Grind' },
+          { title: 'Addicted to the Bean' },
+          { title: 'Bitty & Beau\'s Coffee' },
+          { title: 'Lucky Joe Craft Coffee' },
+          { title: 'Java Dog Coffee House' },
+          { title: 'Morning Glory Coffeehouse' },
+          { title: 'Bespoke Coffee & Dry Goods' },
+          { title: 'Brick + Mortar Coffee and Supply' }
           ]);
-         self.search = 
-          
+        
+var map;
+var markers = [];
 
-       //   self.hideMarkers = function(shops) {
-       //   for (var i = 0; i < markers.length; i++) {
-       //     markers[i].setMap(null);
-       //   }
-       // }
-
-       // function showMarkers() {
-       //   var bounds = new google.maps.LatLngBounds();
-       //   for (var i = 0; i < markers.length; i++) {
-       //     markers[i].setMap(map);
-       //     bounds.extend(markers[i].position);
-       //   }
-       //   map.fitBounds(bounds);
-       // }
-
-     }
-     ko.applyBindings(new AppViewModel());
-
-     var map;
-     var markers = [];
-     var infowindow;
-
-     function initMap() {
+      function initMap() {
 
        map = new google.maps.Map(document.getElementById('map'), {
          center: {lat: 34.2257, lng: -77.9447},
@@ -60,7 +40,7 @@ var styles = require('./styles').styles
          mapTypeConrtol: false
        });
 
-       var coffeeShops = [
+       var locations = [
          {title: 'Port City Java', location: {lat: 34.235912, lng: -77.948826}},
          {title: 'Port City Java', location: {lat: 34.238930, lng: -77.949028}},
          {title: 'Port City Java', location: {lat: 34.237872, lng: -77.921174}},
@@ -70,7 +50,7 @@ var styles = require('./styles').styles
          {title: 'Starbucks', location: {lat: 34.216803, lng: -77.906956}},
          {title: 'Starbucks', location: {lat: 34.242066, lng: -77.828570}},
          {title: 'Starbucks', location: {lat: 34.196443, lng: -77.890236}},
-         {title: 'Bespke Coffee & Dry Goods', location: {lat: 34.236453, lng: -77.947403}},
+         {title: 'Bespoke Coffee & Dry Goods', location: {lat: 34.236453, lng: -77.947403}},
          {title: '24 South Coffee House', location: {lat: 34.234496, lng: -77.948725}},
          {title: 'Karen\'s Cafe', location: {lat: 34.238730, lng: -77.948981}},
          {title: 'Java Dog Coffee House', location: {lat: 34.239104, lng: -77.949228}},
@@ -87,137 +67,95 @@ var styles = require('./styles').styles
          {title: 'Lucky Joe Craft Coffee', location: {lat: 34.266057, lng: -77.837758}}
        ];
 
-       var infowindow = new google.maps.InfoWindow();
 
-       for (var i = 0; i < coffeeShops.length; i++) {
-         var position = coffeeShops[i].location;
-         var title = coffeeShops[i].title;
-         var marker = new google.maps.Marker({
-           position: position,
-           title: title,
-           map: map,
-           id: i
-         });
-         markers.push(marker);
-         marker.addListener('click', function() {
-           infowindow.setContent(this.title);
-           infowindow.open(map, this);
-           infowindow.setPosition(this.position);
 
-         })
-       }
+    
+  }
+   window.initMap = initMap
+   }
+  ko.applyBindings(new AppViewModel());
 
-       document.getElementById('show-shops').addEventListener('click', showMarkers);
 
-       document.getElementById('hide-shops').addEventListener('click', function() {
-         hideMarkers(markers);
-       });
 
-       function showMarkers() {
-         var bounds = new google.maps.LatLngBounds();
-         for (var i = 0; i < markers.length; i++) {
-           markers[i].setMap(map);
-           bounds.extend(markers[i].position);
-         }
-         map.fitBounds(bounds);
-       }
+      //Infowindow
 
-       function hideMarkers(markers) {
-         for (var i = 0; i < markers.length; i++) {
-           markers[i].setMap(null);
-         }
-       }
+       // var infowindow = new google.maps.InfoWindow();
 
-       // APPEND NEARBY:
-
-       // var service = new google.maps.places.PlacesService(map);
-       // service.nearbySearch({
-       //   location: wilm, // <-- NEED SOMETHING FOR LOCATION
-       //   radius: 3218.69,
-       //   types: ['park'],
-       //   // results: ['food']
-       // }, callback);
-       //
-       // function callback(results, status) {
-       //   if (status == google.maps.places.PlacesServiceStatus.OK) {
-       //     for (var i = 0; i < results.length; i++) {
-       //       var place = results[i];
-       //       createMarker(results[i]);
-       //     }
-       //   }
-       // }
-       //
-       // function createMarker(place) {
-       //   var placeLoc = place.geometry.location;
+       // for (var i = 0; i < coffeeShops.length; i++) {
+       //   var position = coffeeShops[i].location;
+       //   var title = coffeeShops[i].title;
        //   var marker = new google.maps.Marker({
+       //     position: position,
+       //     title: title,
        //     map: map,
-       //     position: place.geometry.location
+       //     id: i
        //   });
-       //
-       //   marker.addListener(marker, 'click', function() {
-       //     infowindow.setContent(place.name);
-       //     infowindow.open(map, marker);
-       //   });
+       //   markers.push(marker);
+       //   marker.addListener('click', function() {
+       //     infowindow.setContent(this.title);
+       //     infowindow.open(map, this);
+       //     infowindow.setPosition(this.position);
+
+       //   })
        // }
+
+       
 
        // AUTOCOMPLETE:
 
-       var input = document.getElementById('pac-input');
-       var card = document.getElementById('container');
-       var autocomplete = new google.maps.places.Autocomplete(input);
+       // var input = document.getElementById('pac-input');
+       // var card = document.getElementById('container');
+       // var autocomplete = new google.maps.places.Autocomplete(input);
 
-       autocomplete.bindTo('bounds', map);
-       var infowindowContent = document.getElementById('infowindow-content');
-       infowindow.setContent(infowindowContent);
+       // autocomplete.bindTo('bounds', map);
+       // var infowindowContent = document.getElementById('infowindow-content');
+       // infowindow.setContent(infowindowContent);
 
-       autocomplete.addListener('place_changed', function() {
-         infowindow.close();
-         marker.setVisible(false);
-         var place = autocomplete.getPlace();
-         if (!place.geometry) {
-           window.alert("No details available for input: '" + place.name + "'");
-           return;
-         }
-         // present on map if place has geometry
-         if (place.geometry.viewport) {
-           map.fitBounds(place.geometry.viewport);
-         } else {
-           map.setCenter(place.geometry.location);
-           map.setZoom(13);
-         }
-         marker.setPosition(place.geometry.location);
-         marker.setVisible(true);
+       // autocomplete.addListener('place_changed', function() {
+       //   infowindow.close();
+       //   marker.setVisible(false);
+       //   var place = autocomplete.getPlace();
+       //   if (!place.geometry) {
+       //     window.alert("No details available for input: '" + place.name + "'");
+       //     return;
+       //   }
+       //   // present on map if place has geometry
+       //   if (place.geometry.viewport) {
+       //     map.fitBounds(place.geometry.viewport);
+       //   } else {
+       //     map.setCenter(place.geometry.location);
+       //     map.setZoom(13);
+       //   }
+       //   marker.setPosition(place.geometry.location);
+       //   marker.setVisible(true);
 
-         var address = '';
-         if (place.address_components) {
-           address = [
-           (place.address_components[0] && place.address_components[0].short_name || ''),
-           (place.address_components[1] && place.address_components[1].short_name || ''),
-           (place.address_components[2] && place.address_components[2].short_name || '')
-           ].join(' ');
-         }
-         infowindowContent.children['place-icon'].src = place.icon;
-         infowindowContent.children['place-name'].textContent = place.name;
-         infowindowContent.children['place-address'].textContent = address;
-         infowindow.open(map, marker);
-         infowindow.setContent(infowindowContent);
-       });
+       //   var address = '';
+       //   if (place.address_components) {
+       //     address = [
+       //     (place.address_components[0] && place.address_components[0].short_name || ''),
+       //     (place.address_components[1] && place.address_components[1].short_name || ''),
+       //     (place.address_components[2] && place.address_components[2].short_name || '')
+       //     ].join(' ');
+       //   }
+       //   infowindowContent.children['place-icon'].src = place.icon;
+       //   infowindowContent.children['place-name'].textContent = place.name;
+       //   infowindowContent.children['place-address'].textContent = address;
+       //   infowindow.open(map, marker);
+       //   infowindow.setContent(infowindowContent);
+       // });
 
-       var icon = {
-         // url: place.icon,
-         size: new google.maps.Size(35, 35),
-         origin: new google.maps.Point(0, 0),
-         anchor: new google.maps.Point(15, 34),
-         scaledSize: new google.maps.Size(25, 25)
-       };
-       var marker = new google.maps.Marker({
-         map: map,
-         //icon: icon,
-         animation: google.maps.Animation.DROP,
-         anchorPoint: new google.maps.Point(0, -29)
-       });
+       // var icon = {
+       //   // url: place.icon,
+       //   size: new google.maps.Size(35, 35),
+       //   origin: new google.maps.Point(0, 0),
+       //   anchor: new google.maps.Point(15, 34),
+       //   scaledSize: new google.maps.Size(25, 25)
+       // };
+       // var marker = new google.maps.Marker({
+       //   map: map,
+       //   //icon: icon,
+       //   animation: google.maps.Animation.DROP,
+       //   anchorPoint: new google.maps.Point(0, -29)
+       // });
 
        // STREETVIEW:
-
-     }
-   window.initMap = initMap
