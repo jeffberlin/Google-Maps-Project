7c0795975d969ce382f553809b4150240b698159
Jeff Berlin <jeff@jbrally.com>
2017-03-13 10:20:23 -0400

    Fixed some spacing issues in main.js
    Fixed some spacing issues in main.js

--- Google Maps/app/js/main.js
+++ Google Maps/app/js/main.js
@@ -10,94 +10,94 @@ var styles = require('./styles').styles
       self.shops = ko.observableArray([
         { title: 'Port City Java', 
         locations: [
-            { location: {lat: 34.235912, lng: -77.948826}},
-            { location: {lat: 34.238930, lng: -77.921174}},
-            { location: {lat: 34.237872, lng: -77.921174}},
-            { location: {lat: 34.201974, lng: -77.922590}},
-            { location: {lat: 34.242096, lng: -77.863673}},
-            { location: {lat: 34.194293, lng: -77.910822}}
+            { location: {lat: 34.235912, lng: -77.948826} },
+            { location: {lat: 34.238930, lng: -77.921174} },
+            { location: {lat: 34.237872, lng: -77.921174} },
+            { location: {lat: 34.201974, lng: -77.922590} },
+            { location: {lat: 34.242096, lng: -77.863673} },
+            { location: {lat: 34.194293, lng: -77.910822} }
           ]
         },
         { title: 'Starbucks',
           locations: [
-            { location: {lat: 34.216803, lng: -77.906956}},
-            { location: {lat: 34.242066, lng: -77.828570}},
-            { location: {lat: 34.196443, lng: -77.890236}}
+            { location: {lat: 34.216803, lng: -77.906956} },
+            { location: {lat: 34.242066, lng: -77.828570} },
+            { location: {lat: 34.196443, lng: -77.890236} }
           ]
         },
         { title: 'Folks on Fourth',
           locations: [
-            { location: {lat: 34.243700, lng: -77.945501}}
+            { location: {lat: 34.243700, lng: -77.945501} }
           ]
         },
         { title: '24 South Coffee House',
           locations: [
-            { location: {lat: 34.234496, lng: -77.948725}}
+            { location: {lat: 34.234496, lng: -77.948725} }
           ]
         },
         { title: 'Karen\'s Cafe',
           locations: [
-            { location: {lat: 34.238730, lng: -77.948981}}
+            { location: {lat: 34.238730, lng: -77.948981} }
           ]
         },
         { title: 'Luna Caffè',
           locations: [
-            { location: {lat: 34.228263, lng: -77.940812}}
+            { location: {lat: 34.228263, lng: -77.940812} }
           ]
         },
         { title: 'Folks Cafe',
           locations: [
-            { location: {lat: 34.237704, lng: -77.934188}}
+            { location: {lat: 34.237704, lng: -77.934188} }
           ]
         },
         { title: 'Zola Coffee & Tea',
           locations: [
-            { location: {lat: 34.213228, lng: -77.887951}}
+            { location: {lat: 34.213228, lng: -77.887951} }
           ]
         },
         { title: 'Grinders Caffè',
           locations: [
-            { location: {lat: 34.212560, lng: -77.871677}}
+            { location: {lat: 34.212560, lng: -77.871677} }
           ]
         },
         { title: 'Daily Grind',
           locations: [
-            { location: {lat: 34.241911, lng: -77.867955}}
+            { location: {lat: 34.241911, lng: -77.867955} }
           ]
         },
         { title: 'Addicted to the Bean',
           locations: [
-            { location: {lat: 34.213678, lng: -77.886954}}
+            { location: {lat: 34.213678, lng: -77.886954} }
           ]
         },
         { title: 'Bitty & Beau\'s Coffee',
           locations: [
-            { location: {lat: 34.242041, lng: -77.877485}}
+            { location: {lat: 34.242041, lng: -77.877485} }
           ]
         },
         { title: 'Lucky Joe Craft Coffee',
           locations: [
-            { location: {lat: 34.266057, lng: -77.837758}}
+            { location: {lat: 34.266057, lng: -77.837758} }
           ]
         },
         { title: 'Java Dog Coffee House',
           locations: [
-            { location: {lat: 34.239104, lng: -77.949228}}
+            { location: {lat: 34.239104, lng: -77.949228} }
           ]
         },
         { title: 'Morning Glory Coffeehouse',
           locations: [
-            { location: {lat: 34.225831, lng: -77.929120}}
+            { location: {lat: 34.225831, lng: -77.929120} }
           ]
         },
         { title: 'Bespoke Coffee & Dry Goods',
           locations: [
-            { location: {lat: 34.236453, lng: -77.947403}}
+            { location: {lat: 34.236453, lng: -77.947403} }
           ]
         },
         { title: 'Brick + Mortar Coffee and Supply',
           locations: [
-            { location: {lat: 34.247251, lng: -77.946280}}
+            { location: {lat: 34.247251, lng: -77.946280} }
           ]
         }
       ]); 
